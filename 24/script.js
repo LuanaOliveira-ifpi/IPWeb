18f716e560228170af95ab129c0dd13bb817f0c9
Luana Oliveira <161392228+LuanaOliveira-ifpi@users.noreply.github.com>
2024-09-11 10:26:56 -0300

    Update script.js

--- 24/script.js
+++ 24/script.js
@@ -1,8 +1,8 @@
-
+// Função para alternar a visibilidade dos elementos
 function toggleElements(selector, type, shouldShow) {
     let $elements;
 
-
+    // Seleciona os elementos com base no tipo fornecido
     if (type === 'tag') {
         $elements = $(selector);
     } else if (type === 'class') {
@@ -11,7 +11,7 @@ function toggleElements(selector, type, shouldShow) {
         $elements = $('#' + selector);
     }
 
-  
+    // Mostrar ou esconder os elementos com uma animação suave
     if (shouldShow) {
         $elements.fadeIn('slow');
     } else {
@@ -20,18 +20,19 @@ function toggleElements(selector, type, shouldShow) {
 }
 
 $(document).ready(function() {
-
+    // Configura o evento de clique
     $('.clickable-item').click(function() {
         alert('Você clicou no elemento!');
     });
 
-  
+    // Configura o evento de duplo clique
     $('.dblclick-item').dblclick(function() {
         alert('Você deu um duplo clique no elemento!');
     });
 
- 
+    // Configura o evento de mouse sobre
     $('.mouseover-item').mouseover(function() {
         alert('O mouse está sobre o elemento!');
     });
 });
+
