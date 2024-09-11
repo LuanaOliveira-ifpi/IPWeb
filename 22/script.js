723870e4278e6323b1a020d4cb3b3ef3913cdce3
Luana Oliveira <161392228+LuanaOliveira-ifpi@users.noreply.github.com>
2024-09-11 10:11:48 -0300

    Create script.js

---
+++ 22/script.js
@@ -0,0 +1,34 @@
+function escreverNumeros() {
+    let n = parseInt(prompt("Digite um número:"));
+    let div = document.getElementById("resultado");
+    div.innerHTML = "";
+
+    if (!isNaN(n) && n > 0) {
+        for (let i = 1; i <= n; i++) {
+            div.innerHTML += i + "<br>"; 
+        }
+    } else {
+        div.innerHTML = "Por favor, insira um número válido!";
+    }
+}
+
+
+function calcularRaiz(numero) {
+    if (!isNaN(numero) && numero >= 0) {
+        let raiz = Math.sqrt(numero);
+        alert(A raiz quadrada de ${numero} é ${raiz});
+    } else {
+        alert("Número inválido.");
+    }
+}
+
+function mostrarModal() {
+    let texto = prompt("Digite o texto para o modal:");
+
+    if (texto) {
+        document.getElementById("modalBody").innerText = texto;
+        $('#meuModal').modal('show');
+    } else {
+        alert("Texto não inserido.");
+    }
+}
