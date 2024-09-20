1f04146f530c7de4c9dd70a2714cd50dda4130ea
Luana Oliveira <catzs.2021112linf0123@aluno.ifpi.edu.br>
2024-09-19 22:07:54 -0300

    new

--- prova_final/script.js
+++ prova_final/script.js
@@ -2,17 +2,16 @@ const btnAbrirModal = document.getElementById('btnAbrirModal');
 const modalContato = document.getElementById('modalContato');
 const btnFecharModal = document.getElementById('btnFecharModal');
 
-// Abrir modal
+
 btnAbrirModal.addEventListener('click', function() {
     modalContato.style.display = 'block';
 });
 
-// Fechar modal
+
 btnFecharModal.addEventListener('click', function() {
     modalContato.style.display = 'none';
 });
 
-// Fechar modal ao clicar fora da janela
 window.addEventListener('click', function(event) {
     if (event.target === modalContato) {
         modalContato.style.display = 'none';
